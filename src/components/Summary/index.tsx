9f0d90eafd2e84708a020c98aedf612f047e94e6
Catia Barroco <104831678+CatiaBarroco-xgeeks@users.noreply.github.com>
2023-05-02 15:36:57 +0100

    values format

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,11 +1,11 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
 import { useContext } from 'react';
 import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { priceFormatter } from '../../utils/formatter';
 import { SummaryCard, SummaryContainer } from './styles';
 
 export function Summary() {
     const { transactions } = useContext(TransactionsContext);
-
     const summary = transactions.reduce(
         (acc, transaction) => {
             if (transaction.type === 'income') {
@@ -15,7 +15,6 @@ export function Summary() {
                 acc.outcome += transaction.price;
                 acc.total -= transaction.price;
             }
-
             return acc;
         },
         {
@@ -24,7 +23,6 @@ export function Summary() {
             total: 0,
         },
     );
-
     return (
         <SummaryContainer>
             <SummaryCard>
@@ -33,7 +31,7 @@ export function Summary() {
                     <ArrowCircleUp size={32} color="#00b37e" />
                 </header>
 
-                <strong>{summary.income}</strong>
+                <strong>{priceFormatter.format(summary.income)}</strong>
             </SummaryCard>
 
             <SummaryCard>
@@ -42,7 +40,7 @@ export function Summary() {
                     <ArrowCircleDown size={32} color="#f75a68" />
                 </header>
 
-                <strong>{summary.outcome}</strong>
+                <strong>{priceFormatter.format(summary.outcome)}</strong>
             </SummaryCard>
 
             <SummaryCard variant="green">
@@ -51,7 +49,7 @@ export function Summary() {
                     <CurrencyDollar size={32} color="#fff" />
                 </header>
 
-                <strong>{summary.total}</strong>
+                <strong>{priceFormatter.format(summary.total)}</strong>
             </SummaryCard>
         </SummaryContainer>
     );
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,13 +2,12 @@ import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import { SearchForm } from "../../components/SearchForm";
 
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
-
 export function Transactions() {
     const { transactions } = useContext(TransactionsContext)
-
     return (
         <div>
             <Header />
@@ -23,11 +22,12 @@ export function Transactions() {
                                 <td width="50%">{transaction.description}</td>
                                 <td>
                                     <PriceHighlight variant={transaction.type}>
-                                        {transaction.price}
+                                        {transaction.type === 'outcome' && '- '}
+                                        {priceFormatter.format(transaction.price)}
                                     </PriceHighlight>
                                 </td>
                                 <td>{transaction.category}</td>
-                                <td>{transaction.createdAt}</td>
+                                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                             </tr>
                         )
                     })}
